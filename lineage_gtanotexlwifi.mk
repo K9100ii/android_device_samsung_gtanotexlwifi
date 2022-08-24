8a9bebdc50ee41a89f192335016f6828af8f8c5f
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    gtanotexlwifi: Inherit non_ab_device.mk
    This is required to build applypatch for updating recovery.
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- lineage_gtanotexlwifi.mk
+++ lineage_gtanotexlwifi.mk
@@ -20,6 +20,7 @@ $(call inherit-product, device/samsung/gtanotexlwifi/device.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff.
